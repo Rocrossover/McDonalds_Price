7df7c7909fa5866c0ae2d35a562ef1d86d58495d
Rocrossover <43283190+Rocrossover@users.noreply.github.com>
2020-11-13 10:32:41 -0500

    Add files via upload

--- McDonalds.py
+++ McDonalds.py
@@ -16,8 +16,7 @@ driver = webdriver.Chrome()
 driver.get(url)
 html = driver.execute_script("return document.documentElement.outerHTML")
 sel_soup = soup(html,'html.parser')
-menu_info = sel_soup.findAll("div",{"class":"styles__TextWrapper-sc-1xl58bi-9 latBBX"})
-##angus = sel_soup.find_all("span",string=re.compile("Angus"))
+menu_info = sel_soup.findAll("div",{"class":"styles__TextWrapper-sc-1xl58bi-9 cxewEF"})
 
 name = []
 price = []
@@ -31,7 +30,6 @@ Z = X+" "+Y
 ##McD = openpyxl.Workbook()
 McD = openpyxl.load_workbook('McD_Price.xlsx')
 WS = McD.create_sheet(Z)
-##WS.title = Z
 NAME = WS.cell(row=1, column=1, value="Name")
 PRICE = WS.cell(row=1, column=2, value="Price")
 
@@ -45,15 +43,25 @@ for i in range(len(menu_info)):
     else:
         name.append(info.string)
         price.append(info.parent.parent.next_sibling.next_sibling.find("h3").string)
-        ##row = (name[i],price[i])
-        ##WS.append(row)
-        ##if name[i] != None or price[i] != None:
         try:
             WS.cell(row=i+2,column=1).value = name[i]
             WS.cell(row=i+2,column=2).value = price[i]
             print(i,name[i],price[i])
         except:
             pass
+
+##Remove empty rows in excel
+index_row = []
+for i in range(1, WS.max_row):
+    if WS.cell(i, 1).value is None:
+        # collect indexes of rows
+        index_row.append(i)
+
+for row_del in range(len(index_row)):
+    WS.delete_rows(idx=index_row[row_del], amount=1)
+    # exclude offset of rows through each iteration
+    index_row = list(map(lambda k: k - 1, index_row))
+
 
 McD.save('McD_Price.xlsx')
 McD.close()
